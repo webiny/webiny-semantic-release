7c1d933d5d9779e9c6bd4bc8ab4ad557421906b7
Pavel910 <pavel@webiny.com>
2018-03-07 16:52:57 +0100

    feat: initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import hookStd from "hook-std";
+
+import getLernaPackages from "./utils/getLernaPackages";
+import getSinglePackage from "./utils/getSinglePackage";
+import buildParams from "./utils/buildParams";
+import stdOut from "./utils/stdOut";
+import compose from "./utils/compose";
+
+import verifyEnvironment from "./plugins/verifyEnvironment";
+import analyzeCommits from "./plugins/analyzeCommits";
+import githubVerify from "./plugins/github/verify";
+import githubPublish from "./plugins/github/publish";
+import npmVerify from "./plugins/npm/verify";
+import npmPublish from "./plugins/npm/publish";
+import releaseNotes from "./plugins/releaseNotes";
+import updatePackageJson from "./plugins/updatePackageJson";
+
+export {
+    analyzeCommits,
+    githubVerify,
+    githubPublish,
+    npmVerify,
+    npmPublish,
+    releaseNotes,
+    updatePackageJson,
+    getLernaPackages,
+    getSinglePackage
+};
+
+export default async config => {
+    const { params, plugins } = await buildParams(config);
+
+    // Connect to the stdout and process each line of the output using `stdOut` function
+    const unhook = hookStd({ silent: false }, stdOut);
+    try {
+        await compose([verifyEnvironment(), ...plugins])(params);
+        unhook();
+        return params;
+    } catch (err) {
+        unhook();
+        throw err;
+    }
+};
