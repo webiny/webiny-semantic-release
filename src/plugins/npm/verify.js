10b060cb08803262eaa6786bf5bf2bb109a279dd
Pavel910 <pavel@webiny.com>
2018-03-09 13:42:29 +0100

    fix(npm-verify): write NPM_TOKEN to a local .npmrc file

--- src/plugins/npm/verify.js
+++ src/plugins/npm/verify.js
@@ -1,4 +1,5 @@
 import execa from "execa";
+import fs from "fs-extra";
 
 export default () => {
     return async ({ logger, config }, next) => {
@@ -6,8 +7,14 @@ export default () => {
             return next();
         }
 
+        const { NPM_TOKEN } = process.env;
+        if (!NPM_TOKEN) {
+            throw new Error("ENONPMTOKEN: Missing NPM_TOKEN in process.env!");
+        }
+
         logger.log("Verifying access to NPM...");
         try {
+            await fs.appendFile("./.npmrc", `\n//registry.npmjs.org/:_authToken=${NPM_TOKEN}`);
             await execa("npm", ["whoami"]);
             next();
         } catch (err) {
